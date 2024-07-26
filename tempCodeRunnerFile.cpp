84d3cdbbf997340b13b0cc1c5ba3dac60b1b9c50
Omnicode786 <muzammil.67520@iqra.edu.pk>
2024-07-27 00:57:25 +0500

    well ok

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,26 +1,2 @@
-#include <iostream>
-
-using namespace std;
-int main()
+spaces[8])
 {
-
-    string name;
-    cout << "Enter you name:";
-    getline(cin, name);
-    if (name.length() > 12)
-    {
-        cout << "Your name can't be 12 characters long" << '\n';
-    }
-    else if (name.empty())
-    {
-        cout << "You dint enter any name" << '\n';
-    }
-
-    else
-    {
-        cout << "Welcome " << name << '\n';
-    }
-    name.clear();
-
-    cout << "Welcome " << name << '\n';
-}
